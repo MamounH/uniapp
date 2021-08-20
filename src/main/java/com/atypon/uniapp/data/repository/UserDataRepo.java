a8047c1224bcaa7531e73045dafde95f2c1ba856
MamounH <mhabukoush16@cit.just.edu.jo>
2021-08-20 09:09:37 +0300

    CI/CD Test

--- src/main/java/com/atypon/uniapp/data/repository/UserDataRepo.java
+++ src/main/java/com/atypon/uniapp/data/repository/UserDataRepo.java
@@ -2,10 +2,8 @@ package com.atypon.uniapp.data.repository;
 
 import com.atypon.uniapp.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
 
